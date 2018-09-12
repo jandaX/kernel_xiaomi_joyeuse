ad1d9d6e7e3fdc9e8872184fc50e1626b86210a6
Sunil Paidimarri <hisunil@codeaurora.org>
2018-09-12 14:37:23 -0700

    data-kernel: EMAC: Disconnect IPA offload before stopping MAC
    Disconnect IPA offload before stopping MAC RX/TX.
    
    Change-Id: I6b1df88fbbcd750b1e8e2e7b727b40208dadf5a5
    Acked-by: Rahul Kawadgave <rahulak@qti.qualcomm.com>
    Signed-off-by: Sunil Paidimarri <hisunil@codeaurora.org>

--- drivers/emac-dwc-eqos/DWC_ETH_QOS_drv.c
+++ drivers/emac-dwc-eqos/DWC_ETH_QOS_drv.c
@@ -2003,6 +2003,9 @@ static int DWC_ETH_QOS_close(struct net_device *dev)
 	netif_tx_disable(dev);
 	DWC_ETH_QOS_stop_all_ch_tx_dma(pdata);
 
+	if (pdata->ipa_enabled)
+		DWC_ETH_QOS_ipa_offload_event_handler(pdata, EV_DEV_CLOSE);
+
 	/* Disable MAC TX/RX */
 	hw_if->stop_mac_tx_rx();
 
@@ -2010,9 +2013,6 @@ static int DWC_ETH_QOS_close(struct net_device *dev)
 	DWC_ETH_QOS_stop_all_ch_rx_dma(pdata);
 	DWC_ETH_QOS_all_ch_napi_disable(pdata);
 
-	if (pdata->ipa_enabled) {
-		DWC_ETH_QOS_ipa_offload_event_handler(pdata, EV_DEV_CLOSE);
-	}
 #endif /* end of DWC_ETH_QOS_CONFIG_PGTEST */
 
 #ifdef DWC_ETH_QOS_TXPOLLING_MODE_ENABLE
