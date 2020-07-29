ba102b3aabe1319915844021d5489292d201e10a
Ray Zhang <rayz@codeaurora.org>
2020-07-29 19:02:08 +0800

    Revert "disp: msm: sde: wait for specific pp_done instead of zero"
    This reverts commit 1cb8e403b4552d974e0a902c935de274de63a154.
    
    Change-Id: I7934a30d838225e517bfd4b7256e86a2838c1bcc
    Signed-off-by: Ray Zhang <rayz@codeaurora.org>

--- drivers/gpu/drm/msm/sde/sde_encoder_phys_cmd.c
+++ drivers/gpu/drm/msm/sde/sde_encoder_phys_cmd.c
@@ -747,7 +747,6 @@ static int _sde_encoder_phys_cmd_wait_for_idle(
 	bool recovery_events;
 	int ret;
 	struct sde_hw_ctl *ctl;
-	bool wr_ptr_wait_success = true;
 
 	if (!phys_enc) {
 		SDE_ERROR("invalid encoder\n");
@@ -755,11 +754,7 @@ static int _sde_encoder_phys_cmd_wait_for_idle(
 	}
 
 	ctl = phys_enc->hw_ctl;
-
-	if (sde_encoder_phys_cmd_is_master(phys_enc))
-		wr_ptr_wait_success = cmd_enc->wr_ptr_wait_success;
-
-	if (wr_ptr_wait_success &&
+	if (cmd_enc->wr_ptr_wait_success &&
 	  (phys_enc->frame_trigger_mode == FRAME_DONE_WAIT_POSTED_START) &&
 	  ctl->ops.get_scheduler_status &&
 	  (ctl->ops.get_scheduler_status(ctl) & BIT(0)) &&
@@ -807,7 +802,7 @@ static int _sde_encoder_phys_cmd_wait_for_autorefresh_done(
 {
 	struct sde_encoder_phys_cmd *cmd_enc =
 			to_sde_encoder_phys_cmd(phys_enc);
-	struct sde_encoder_wait_info wait_info = {NULL};
+	struct sde_encoder_wait_info wait_info;
 	int ret = 0;
 
 	if (!phys_enc) {
@@ -1438,7 +1433,7 @@ static int _sde_encoder_phys_cmd_wait_for_ctl_start(
 {
 	struct sde_encoder_phys_cmd *cmd_enc =
 			to_sde_encoder_phys_cmd(phys_enc);
-	struct sde_encoder_wait_info wait_info = {NULL};
+	struct sde_encoder_wait_info wait_info;
 	int ret;
 	bool frame_pending = true;
 	struct sde_hw_ctl *ctl;
@@ -1564,7 +1559,7 @@ static int sde_encoder_phys_cmd_wait_for_vblank(
 {
 	int rc = 0;
 	struct sde_encoder_phys_cmd *cmd_enc;
-	struct sde_encoder_wait_info wait_info = {NULL};
+	struct sde_encoder_wait_info wait_info;
 
 	if (!phys_enc)
 		return -EINVAL;
