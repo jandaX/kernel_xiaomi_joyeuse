b8a356f965b239166aadaaef21512d0157aa7901
Sultan Alsawaf <sultan@kerneltoast.com>
2020-12-25 21:39:23 -0800

    treewide: Suppress overly verbose log spam
    This tames quite a bit of the log spam and makes dmesg readable.
    
    Uses work from Danny Lin <danny@kdrag0n.dev>.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Change-Id: I9fb8a77d523f21145f853de53751788d4f71af90
    Signed-off-by: Angga <angga@linuxmail.org>

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -705,7 +705,9 @@ void exit_oom_victim(void)
 void oom_killer_enable(void)
 {
 	oom_killer_disabled = false;
-	pr_info("OOM killer enabled.\n");
+#ifdef CONFIG_DEBUG_KERNEL
+	pr_debug("OOM killer enabled.\n");
+#endif
 }
 
 /**
@@ -742,8 +744,9 @@ bool oom_killer_disable(signed long timeout)
 		oom_killer_enable();
 		return false;
 	}
-	pr_info("OOM killer disabled.\n");
-
+#ifdef CONFIG_DEBUG_KERNEL
+	pr_debug("OOM killer disabled.\n");
+#endif
 	return true;
 }
 
