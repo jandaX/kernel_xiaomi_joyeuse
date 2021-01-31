bb6cdcea488c10e0a0e64e9054db5df6b8b27f68
anulalat <ndra.bodoh@gmail.com>
2021-01-31 19:43:01 +0800

    Merge tag 'LA.UM.9.1.r1-08300-SMxxx0.0' of https://source.codeaurora.org/quic/la/kernel/msm-4.14 into rebase
    "LA.UM.9.1.r1-08300-SMxxx0.0"

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -647,6 +647,9 @@ static int dsi_panel_update_backlight(struct dsi_panel *panel,
 	set_rgb_slider(bl_lvl);
 #endif
 
+	if (panel->bl_config.bl_inverted_dbv)
+		bl_lvl = (((bl_lvl & 0xff) << 8) | (bl_lvl >> 8));
+
 	rc = mipi_dsi_dcs_set_display_brightness(dsi, bl_lvl);
 	if (rc < 0)
 		pr_err("failed to update dcs backlight:%d\n", bl_lvl);
@@ -2347,6 +2350,9 @@ static int dsi_panel_parse_bl_config(struct dsi_panel *panel)
 	} else {
 		panel->bl_config.brightness_default_level = val;
 	}
+
+	panel->bl_config.bl_inverted_dbv = utils->read_bool(utils->data,
+		"qcom,mdss-dsi-bl-inverted-dbv");
 
 	if (panel->bl_config.type == DSI_BACKLIGHT_PWM) {
 		rc = dsi_panel_parse_bl_pwm_config(panel);
