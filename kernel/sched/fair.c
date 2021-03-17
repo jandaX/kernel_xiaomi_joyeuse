093f2150442c57843dc8b4e236c1479c73514b22
anulalat <ndra.bodoh@gmail.com>
2021-03-17 17:58:22 +0800

    Revert "kernel: sched: Mitigate non-boosted tasks preempting boosted tasks"
    This reverts commit 6ed794c6da7273cdcaed3d490ab6db5395bb2a8c.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7582,7 +7582,6 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 	bool next_group_higher_cap = false;
 	int isolated_candidate = -1;
 	int mid_cap_orig_cpu = cpu_rq(smp_processor_id())->rd->mid_cap_orig_cpu;
-	struct task_struct *curr_tsk;
 	bool prioritized_task = prefer_high_cap && p->prio <= DEFAULT_PRIO;
 
 	*backup_cpu = -1;
@@ -8029,13 +8028,6 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 	 *   a) ACTIVE CPU: target_cpu
 	 *   b) IDLE CPU: best_idle_cpu
 	 */
-	if (target_cpu != -1 && !idle_cpu(target_cpu) &&
-			best_idle_cpu != -1) {
-		curr_tsk = READ_ONCE(cpu_rq(target_cpu)->curr);
-		if (curr_tsk && schedtune_prefer_high_cap(curr_tsk)) {
-			target_cpu = best_idle_cpu;
-		}
-	}
 
 	if (target_cpu == -1)
 		target_cpu = prefer_idle
