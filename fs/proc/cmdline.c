47b81e232c6b07aaef4b90c6d2b4f19ffd521620
anulalat <ndra.bodoh@gmail.com>
2022-02-27 06:12:47 +0000

    Revert "cmdline: extend replace for veritymode 'enforcing' SafetyNet"
    This reverts commit 44458db4134e422608c451085d7213e6ed1535d8.

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -25,7 +25,6 @@ static const struct file_operations cmdline_proc_fops = {
 	.release	= single_release,
 };
 
-#ifdef REMOVE_SAFETYNET_FLAGS
 static void remove_flag(char *cmd, const char *flag)
 {
 	char *start_addr, *end_addr;
@@ -44,12 +43,8 @@ static void remove_safetynet_flags(char *cmd)
 {
 	remove_flag(cmd, "androidboot.veritymode=");
 }
-#endif
 
 #if 1
-
-static char *padding = "                ";
-
 static void replace_flag(char *cmd, const char *flag, const char *flag_new)
 {
 	char *start_addr, *end_addr;
@@ -57,18 +52,8 @@ static void replace_flag(char *cmd, const char *flag, const char *flag_new)
 	/* Ensure all instances of a flag are replaced */
 	while ((start_addr = strstr(cmd, flag))) {
 		end_addr = strchr(start_addr, ' ');
-		if (end_addr) {
-			if (strlen(flag)<strlen(flag_new)) {
-				// xx yy=a zz
-				//    ^   ^
-				// xx yy=bb zz
-				int length_to_copy = strlen( start_addr + (strlen(flag) ) ) + 1; // +1 to copy trailing '/0'
-				int length_diff = strlen(flag_new)-strlen(flag);
-				memcpy(start_addr+(strlen(flag)+length_diff), start_addr+(strlen(flag)), length_to_copy);
-				memcpy(start_addr+(strlen(flag)), padding, length_diff);
-			}
-			memcpy(start_addr, flag_new, strlen(flag_new));
-		}
+		if (end_addr)
+			memcpy(start_addr, flag_new, strlen(flag));
 		else
 			*(start_addr - 1) = '\0';
 	}
@@ -85,13 +70,6 @@ static void replace_safetynet_flags(char *cmd)
 			  "androidboot.secboot=enabled ");
 	replace_flag(cmd, "androidboot.verifiedbootstate=orange",
 			  "androidboot.verifiedbootstate=green ");
-#ifndef REMOVE_SAFETYNET_FLAGS
-	replace_flag(cmd, "androidboot.veritymode=logging",
-			  "androidboot.veritymode=enforcing");
-	replace_flag(cmd, "androidboot.veritymode=eio",
-			  "androidboot.veritymode=enforcing");
-#endif
-
 }
 #endif
 
@@ -104,9 +82,7 @@ static int __init proc_cmdline_init(void)
 	 * pass SafetyNet CTS check.
 	 */
 	replace_safetynet_flags(new_command_line);
-#ifdef REMOVE_SAFETYNET_FLAGS
 	remove_safetynet_flags(new_command_line);
-#endif
 
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
 	return 0;
